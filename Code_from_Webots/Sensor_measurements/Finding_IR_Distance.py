53fdb55c4853b01700169de8bfbeda8507751586
ErshovVladislav10M <yersh2000@gmail.com>
2021-06-09 00:11:07 +0300

    Update Finding_IR_Distance.py

--- Code_from_Webots/Sensor_measurements/Finding_IR_Distance.py
+++ Code_from_Webots/Sensor_measurements/Finding_IR_Distance.py
@@ -1,14 +1,12 @@
-import math
-import struct   
 import numpy
 import matplotlib.pyplot as plt
 
 
 # Преобразование данных дистанционного сенсора в расстояние
 def transf_ds(volt):
-    volt_val = [0.45, 0.47, 0.5, 0.55, 0.61, 0.69, 0.74, 
+    volt_val = [0.45, 0.47, 0.5, 0.55, 0.61, 0.69, 0.74,
                 0.8, 0.87, 1.04, 1.27, 1.54, 1.99, 2.48, 2.7]
-    dist_val = [150, 140, 130, 120, 110, 100, 90, 
+    dist_val = [150, 140, 130, 120, 110, 100, 90,
                 80, 70, 60, 50, 40, 30, 20, 15]
     return numpy.interp(volt, volt_val, dist_val)
 
@@ -18,7 +16,7 @@ B = []
 for i in range(10):
     A.append(2.7 - i * 0.25)
     B.append(transf_ds(2.7 - i * 0.25))
-    
+
 volt_val = [0.45, 0.47, 0.5, 0.55, 0.61, 0.69, 0.74, 
                 0.8, 0.87, 1.04, 1.27, 1.54, 1.99, 2.48, 2.7]
 dist_val = [150, 140, 130, 120, 110, 100, 90, 
