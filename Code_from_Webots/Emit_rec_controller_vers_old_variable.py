96087930919a59e1d46d3f9ce6c91d7b4d871501
ErshovVladislav10M <yersh2000@gmail.com>
2021-06-08 23:10:31 +0300

    Add code about term pyper

---
+++ Code_from_Webots/Emit_rec_controller_vers_old_variable.py
@@ -0,0 +1,91 @@
+from controller import Robot
+from controller import Emitter
+from controller import Receiver
+from random import randint
+import math
+import struct   
+import random
+
+
+# create the Robot instance.
+robot = Robot()
+
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+
+# initialize emmiters    
+emit = robot.getEmitter('emit')
+
+emit_main = robot.getEmitter('emit_main')
+
+
+# Вводим количество членов группы не включая самого робота (т.е. n-1)
+k = 2
+
+
+# initialize receiver 
+rec = []  
+for i in range(k):
+    rec.append(robot.getReceiver('rec' + str(i + 1)))
+    rec[i].enable(timestep)
+    
+rec_main = robot.getReceiver('rec_main')
+rec_main.enable(timestep)
+
+
+# Курс и уверенность в курсе
+bearing = 0
+q = 0
+
+
+# Список для приема сообщений
+bearingn = [[0] * k, [0] * k]
+
+
+# Время переключения между роботами для приема сообщений
+time_wait_switch = 0
+
+
+# Номер робота, от которого принимаем сообщение
+index_robot_rec = 0
+
+
+# Main loop:
+while robot.step(timestep) != -1:
+
+    # Принимаем сообщение от основного микрочипа
+    if rec_main.getQueueLength() > 0:
+        message = rec_main.getData()
+        dataList = struct.unpack("dd", message)
+        bearing = dataList[0]
+        q = dataList[1]
+        rec_main.nextPacket()
+    #print(bearing)
+    
+    # Передаем сообщение соседям
+    p = random.uniform(0, 1)
+    if p > 0.04:
+        message = struct.pack("dd", bearing, q)
+        emit.send(message)
+    
+    # Принимаем сообщение от соседей
+    if time_wait_switch != 0:
+        time_wait_switch -= 1
+    elif rec[index_robot_rec].getQueueLength() > 0:
+        message = rec[index_robot_rec].getData()
+        dataList = struct.unpack("dd", message)
+        bearingn[index_robot_rec][0] = dataList[0]
+        bearingn[index_robot_rec][1] = dataList[1]
+        rec[index_robot_rec].nextPacket()
+        
+        index_robot_rec += 1
+        index_robot_rec %= k
+        time_wait_switch = 2
+    #print("Robot", *bearingn)
+    
+    # Перадаем сообщение на основной микрочип
+    for i in range(k):
+        message = struct.pack("dd", bearingn[i][0], bearingn[i][1])          
+        emit_main.send(message)
---
+++ Code_from_Webots/Sensor_measurements/Finding_the_angle_by_photoresistors.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import math
+import numpy
+import os
+
+
+Y = [[], [], [], []]
+
+file_name = os.getcwd()
+
+file = open(file_name + '\test_light_angle\four_value_light.txt', 'r')
+i = 0
+for line in file:
+    Y[i].append(float(line))
+    i += 1
+    i %= 4
+
+
+def comp_angle(light):
+    if max(light) == sum(light):
+        return light.index(max(light)) * 90 + 45
+
+    if light[0] + light[1] == 0:
+        real_l1 = light[2]
+        real_l2 = light[3]
+        angle_sector = 225
+    if light[1] + light[2] == 0:
+        real_l1 = light[3]
+        real_l2 = light[0]
+        angle_sector = 315
+    if light[2] + light[3] == 0:
+        real_l1 = light[0]
+        real_l2 = light[1]
+        angle_sector = 45
+    if light[3] + light[0] == 0:
+        real_l1 = light[1]
+        real_l2 = light[2]
+        angle_sector = 135
+
+    return (math.atan(real_l2 / real_l1) * 180 / math.pi + angle_sector) % 360
+
+
+X = [180 - 180 * i / len(Y[0]) for i in range(len(Y[0]))]
+A = [90 - 180 * i / len(Y[0]) for i in range(len(Y[0]))]
+B = []
+
+for i in range(len(Y[0])):
+    light = []
+    for j in range(4):
+        light.append(Y[j][i])
+
+    if comp_angle(light) > 180:
+        B.append(comp_angle(light) - 360)
+    else:
+        B.append(comp_angle(light))
+
+plt.plot(X, A, color = 'r', marker = '', linestyle = '-', markerfacecolor = 'r', label = 'Реальный угол')
+plt.plot(X, B, color = 'b', marker = '', linestyle = '--', markerfacecolor = 'b', label = 'Высчитанный угол по формуле')
+plt.xlabel('Время', fontsize = 14)
+plt.ylabel('Угол направления на свет', fontsize = 14)
+plt.title('Сравнение реального и вычисленного угла', fontsize = 15)
+plt.legend(loc = 'best')
+plt.show()
+
+print(max([abs(A[i] - B[i]) for i in range(len(A))]))
