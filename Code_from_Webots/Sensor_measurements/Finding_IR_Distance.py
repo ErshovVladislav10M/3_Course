a3d2a3ec9b2c7fb5c804308fd5f2a921b5cb27b6
ErshovVladislav10M <yersh2000@gmail.com>
2021-06-09 00:17:30 +0300

    Update Finding_IR_Distance.py

--- Code_from_Webots/Sensor_measurements/Finding_IR_Distance.py
+++ Code_from_Webots/Sensor_measurements/Finding_IR_Distance.py
@@ -17,11 +17,11 @@ for i in range(10):
     A.append(2.7 - i * 0.25)
     B.append(transf_ds(2.7 - i * 0.25))
 
-volt_val = [0.45, 0.47, 0.5, 0.55, 0.61, 0.69, 0.74, 
+volt_val = [0.45, 0.47, 0.5, 0.55, 0.61, 0.69, 0.74,
                 0.8, 0.87, 1.04, 1.27, 1.54, 1.99, 2.48, 2.7]
-dist_val = [150, 140, 130, 120, 110, 100, 90, 
+dist_val = [150, 140, 130, 120, 110, 100, 90,
             80, 70, 60, 50, 40, 30, 20, 15]
-            
+
 plt.plot(A, B, color = 'red', marker = 'o', linestyle = '--')
 plt.plot(volt_val, dist_val, color = 'blue', marker = 'o', linestyle = '--')
 
